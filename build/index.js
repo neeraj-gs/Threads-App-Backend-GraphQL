e60d0667548ebe4ed1c150100880e4b17b296f79
neeraj-gs <gsneeraj05@gmail.com>
2023-11-13 19:14:48 +0530

    Setup the scripts such that on dev it complies and tehn automatically starts the scri[ts

---
+++ build/index.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const PORT = Number(process.env.PORT) || 8000;
+app.get('/', (req, res) => {
+    res.json({ message: 'Server is Running' });
+});
+app.listen(PORT, () => {
+    console.log('Server is Running at ${PORT}');
+});
+//for any trs file we need to first compile or buidl it and tehn run the code
