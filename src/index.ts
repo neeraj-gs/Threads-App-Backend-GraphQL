f867b50ceebcb1e927d828e8ecae1fb329ae7651
neeraj-gs <gsneeraj05@gmail.com>
2023-11-13 19:24:41 +0530

    Added a a new fucnoitn adnd wrote all code insid it to use await on the gql server

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 
 import express from 'express'
-const app =express();
+
+async function init(){
+    const app =express();
 const PORT = Number(process.env.PORT) || 8000
 
 //Create Graph QL Server
@@ -11,12 +13,19 @@ const gqlServer = new ApolloServer({
     resolvers:{},
 })
 
+await gqlServer.start();
+
 
 app.get('/',(req,res)=>{
     res.json({message:'Server is Running'})
 })
+//we cant await at global level so write entire code into a fucntion and run inside it
 
 app.listen(PORT,()=>{
     console.log(`Server is Running at http://localhost:${PORT}`)
 })
 //for any trs file we need to first compile or buidl it and tehn run the code
+
+}
+
+init();
