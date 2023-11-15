bc2ab83dfffe427ff77458c1aad802b36d22c9ae
neeraj-gs <gsneeraj05@gmail.com>
2023-11-15 10:01:27 +0530

    Added difffrent files for threads app need to mimplemetn hte queries and mutations

--- src/graphql/threads/resolvers.ts
+++ src/graphql/threads/resolvers.ts
@@ -1,29 +1,8 @@
-import UserService, { CreateUserPayload } from "../../services/user"
 
 const queries = {
-    getUserToken: async (_:any,payload:{email:string,password:string})=>{
-        const token = await UserService.getUserToken({
-            email:payload.email,
-            password:payload.password
-        })
-        return token
-    },
-
-    getCurrentUser: async(_:any,parameters:any,context:any)=>{
-        
-        if(context && context.user){
-            const id = context.user.id
-            const user = await UserService.getUserById(id);
-            return user;
-        }
-        throw new Error("Not implemented")
-    }
+    
 }
 const mutations = {
-    createUser: async(_:any,payload:CreateUserPayload)=>{
-        const res = await UserService.createUser(payload)
-        return res.id;
-    }
 }
 
 
--- src/graphql/threads/typedef.ts
+++ src/graphql/threads/typedef.ts
@@ -1,10 +1,6 @@
 export const typeDefs = `#graphql 
-    type User{
-        id: ID!
-        firstName: String!
-        lastName: String
-        profileImageURL: String
-        email: String!
+    type Thread{
+        
         
     }
 
