38f45004177020a4fbad6811542500abe040647b
neeraj-gs <gsneeraj05@gmail.com>
2023-11-15 08:52:06 +0530

    Started working with tokens and login system

--- src/services/user.ts
+++ src/services/user.ts
@@ -9,12 +9,24 @@ export interface CreateUserPayload{
     password: string;
 }
 
+export interface GetUserTokenPayload{
+    email: string;
+    password: string;
+}
+
 
 class UserService {
+
+
+    private static generateHash(salt: string,password:string){
+        const hashpwd = createHmac('sha256', salt).update(password).digest('hex') //uses this salt and then store hte password
+        return hashpwd
+    }
+
     public static createUser(payload:CreateUserPayload){
         const {firstName, lastName, email, password} = payload
         const salt = randomBytes(32).toString('hex');
-        const hashpwd = createHmac('sha256', salt).update(password).digest('hex') //uses this salt and then store hte password
+        const hashpwd = UserService.generateHash(salt,password)
         return prismaClient.user.create({
             data:{
                 firstName,
@@ -24,6 +36,34 @@ class UserService {
                 password : hashpwd
             }
         });
+
+    }
+
+    private static getUserByEmail(email: string){
+        return prismaClient.user.findUnique({
+            where:{
+                email:email
+            }
+        })
+    }
+
+    public static async getUserToken(payload:GetUserTokenPayload){
+        const {email, password} = payload;
+        //cehck if password is correnct or not
+        const user = await UserService.getUserByEmail(email);
+        if(!user){
+            throw new Error(`User not found`)
+        }
+        const userSalt = user.salt;
+        const userHashPswd = UserService.generateHash(userSalt,password)
+
+        if(userHashPswd !== user.password){
+            throw new Error(`Incorrect Password`)
+        }
+
+        //generate token if succesufrl
+
+
 
     }
 
